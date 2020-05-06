f5ad9d0511c3f7eaad555a1f697148c38984925d
seizze <cwcwc3@gmail.com>
2020-05-06 11:51:03 +0900

    [#6] chore: 새 프로젝트 생성

---
+++ iOS/Baseball/Baseball/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Baseball
+//
+//  Created by Chaewan Park on 2020/05/06.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS/Baseball/Baseball/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Baseball
+//
+//  Created by Chaewan Park on 2020/05/06.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
