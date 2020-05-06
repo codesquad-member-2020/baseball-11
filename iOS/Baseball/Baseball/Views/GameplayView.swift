492731f98f4f5360a5665971258985ae09b74597
seizze <cwcwc3@gmail.com>
2020-05-06 12:13:41 +0900

    [#6] feat: Gameplay view 생성

---
+++ iOS/Baseball/Baseball/Views/GameplayView.swift
@@ -0,0 +1,22 @@
+//
+//  GameplayView.swift
+//  Baseball
+//
+//  Created by Chaewan Park on 2020/05/06.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GameplayView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
