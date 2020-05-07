97acd87ad98510d97854c4fdfb4c48869c676531
sgdg <seungdeng17@gmail.com>
2020-05-07 23:52:01 +0900

    style: 모든 폰트 조선궁서체로 통일

--- FE/client/src/styles/GlobalStyle.js
+++ FE/client/src/styles/GlobalStyle.js
@@ -8,13 +8,6 @@ const fonts = css`
     font-weight: normal; 
     font-style: normal; 
     }
-
-@font-face { 
-    font-family: 'Cafe24Oneprettynight'; 
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Oneprettynight.woff') format('woff'); 
-    font-weight: normal; 
-    font-style: normal; 
-    }
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -22,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
   ${fonts}
 
   body {
-    font-family: "Cafe24Oneprettynight", "ChosunGs", -apple-system, sans-serif;
+    font-family: "ChosunGs", -apple-system, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     width : 100%;
