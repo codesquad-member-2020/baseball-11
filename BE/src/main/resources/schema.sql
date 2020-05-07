9afa6a1b90f6cfefa70a8274d892b9278f449f20
Sosah <thstkd92@gmail.com>
2020-05-07 23:54:51 +0900

    [#15] chore: DB 스키마 재설계
    - Wiki에 올려놓은 API 문서에 맞는 데이터베이스 설계
    - pitch라는 하나의 테이블 추가. 공 던질때마다 해당 스테이터스를 설정
    - flyway를 설정했으나 이번 프로젝트에 크게 필요하지 않을듯 하여 gradle과 properties 삭제

---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,66 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS team;
+DROP TABLE IF EXISTS hitter;
+DROP TABLE IF EXISTS pitcher;
+DROP TABLE IF EXISTS ball_count;
+DROP TABLE IF EXISTS score_board;
+
+CREATE TABLE user
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(128) UNIQUE
+);
+
+CREATE TABLE team
+(
+    id             INT PRIMARY KEY AUTO_INCREMENT,
+    name           VARCHAR(16) NOT NULL,
+    stadium        VARCHAR(8),
+    user_id        INT REFERENCES user (id),
+    score_board_id INT REFERENCES score_board (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE score_board
+(
+    id      INT PRIMARY KEY,
+    user_id INT REFERENCES user (id),
+    chance  TINYINT,
+    inning  INT,
+    score   INT
+);
+
+CREATE TABLE hitter
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    name          VARCHAR(16),
+    batting_order INT,
+    batter_box    INT,
+    hit           INT,
+    out_count     INT,
+    avg           DOUBLE,
+    team_id       INTEGER REFERENCES team (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE pitcher
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT,
+    name    VARCHAR(16),
+    team_id INT REFERENCES team (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE pitch
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    pitcher_id   INT REFERENCES pitcher (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    hitter_id    INT REFERENCES hitter (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    pitch_status VARCHAR(64)
+);
+
+CREATE TABLE ball_count
+(
+    id        INT PRIMARY KEY auto_increment,
+    strike    INT,
+    ball      INT,
+    out_count INT,
+    hitter_id INT REFERENCES hitter (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
