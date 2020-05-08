a5d67be132818aa21b09dd2259eb421c4028f2bf
sgdg <seungdeng17@gmail.com>
2020-05-08 12:14:07 +0900

    [#12] feat: 스코어 보드에 볼 카운트, 투구수 렌더링 기능 추가

--- FE/client/src/components/match/MatchInfo.jsx
+++ FE/client/src/components/match/MatchInfo.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 import styled from 'styled-components';
 
 const ScoreBoard = styled.div`
-    border : 1px solid #5a5a5a;
+    outline : 1px solid #5a5a5a;
     position : absolute;
     top : 30px;
     left : 30px;
     width : 300px;
     height : 150px;
-    background-color : #ccc;
+    color : #fff;
     text-align : center;
     & .top-board {
         display : flex;
         height : 75%;
         & .team-info {
-        color : #fff; 
-        width : 30%;
+        width : 32%;
         display : flex;
         flex-direction : column;
             & .score {
@@ -48,7 +47,7 @@ const ScoreBoard = styled.div`
         & .inning-info {
             position : relative;
             background-color : #000;
-            width : 40%;
+            width : 36%;
             & .base-info {
                 position : absolute;
                 width : 25px;
@@ -57,25 +56,58 @@ const ScoreBoard = styled.div`
                 transform : rotate(45deg);
                 &.first-base {
                     top : 35px;
-                    right : 25px;
+                    right : 20px;
                 }
                 &.second-base {
                     top : 15px;
-                    left : 50%;
-                    transform : translateX(-50%) rotate(45deg);
+                    right : 50%;
+                    transform : translateX(50%) rotate(45deg);
                 }
                 &.third-base {
                     top : 35px;
-                    left : 25px;
+                    left : 20px;
                 }
             }
             & .inning-info-text {
-                color : #fff;
+                width : 100px;
                 position : absolute;
                 bottom : 10px;
                 left : 50%;
                 transform : translateX(-50%);
                 font-size : 25px;
+            }
+        }
+    }
+    & .bottom-board {
+        display : flex;
+        height : 25%;
+        font-size : 22px;
+        & .ball-count {
+            width : 64%;
+            background-color : #000;
+            display : flex;
+            justify-content : space-around;
+            align-items : center;
+            & .b-count-text {
+                margin-right : 3px;
+            }
+            & .b-count {
+                width : 15px;
+                height : 15px;
+                display : inline-block;
+                background-color : #5a5a5a;
+                border-radius : 50%;
+                margin-left : 1.5px;
+            }
+        }
+        & .pitches-count {
+            width : 36%;
+            background-color : #5a5a5a;
+            display : flex;
+            justify-content : center;
+            align-items : center;
+            & .p-count {
+                margin-left : 10px;
             }
         }
     }
@@ -103,10 +135,26 @@ const MatchInfo = () => {
             </div>
             <div className='bottom-board'>
                 <div className='ball-count'>
-
+                    <div>
+                        <span className='b-count-text'>B</span>
+                        <span className='b-count' />
+                        <span className='b-count' />
+                        <span className='b-count' />
+                    </div>
+                    <div>
+                        <span className='b-count-text'>S</span>
+                        <span className='b-count' />
+                        <span className='b-count' />
+                    </div>
+                    <div>
+                        <span className='b-count-text'>O</span>
+                        <span className='b-count' />
+                        <span className='b-count' />
+                    </div>
                 </div>
                 <div className='pitches-count'>
-
+                    <div>⚾</div>
+                    <div className='p-count'>0</div>
                 </div>
             </div>
         </ScoreBoard>
