9cecf7a3d9c12be6b385607399508ad274b8c7f4
sgdg <seungdeng17@gmail.com>
2020-05-08 10:53:19 +0900

    style: NotFound 페이지 inner style 추가

--- FE/client/src/components/NotFound.jsx
+++ FE/client/src/components/NotFound.jsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Wrap = styled.div`
+const NotFoundWrap = styled.div`
     width : 100%;
     height : 100%;
     background : url('/images/bonobono.jpg') no-repeat;
     background-size : 100% 100%;
+`;
+
+const NotFoundInner = styled.div`
+    width : 100%;
+    height : 100%;
+    background-color : #00000099;
     display : flex;
     align-items : center;
     justify-content : center;
@@ -17,7 +23,7 @@ const Title = styled.div`
     font-size : 55px;
     font-weight : 600;
     margin-bottom : 80px;
-    background-image: linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red);   
+    background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);   
     -webkit-background-clip: text;
     color: transparent;
 `;
@@ -27,9 +33,11 @@ const NotFound = () => {
     setTimeout(() => setWait(false), 4000);
 
     return (
-        <Wrap>
-            {wait ? <Title>존재하지 않는 페이지 입니다. 메인 페이지로 이동 합니다.</Title> : <Redirect to='/' />}
-        </Wrap>
+        <NotFoundWrap>
+            <NotFoundInner>
+                {wait ? <Title>존재하지 않는 페이지 입니다. 메인 페이지로 이동 합니다.</Title> : <Redirect to='/' />}
+            </NotFoundInner>
+        </NotFoundWrap>
     )
 }
 
