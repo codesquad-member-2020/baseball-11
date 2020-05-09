6570d3b7fe47908efa36b1ee1a5c43f84f8cdc27
sgdg <seungdeng17@gmail.com>
2020-05-09 23:42:54 +0900

    modify: 임시 mock 데이터 객체리터럴 형태로 수정

--- FE/client/src/store/BaseballStore.jsx
+++ FE/client/src/store/BaseballStore.jsx
@@ -1,32 +1,25 @@
 import React, { useReducer, createContext } from 'react'
 
-import * as mock from '../mock/mock';
+import * as mock from '../mock/mockData';
 
 export const BaseBallContext = createContext();
 
 const BaseballStore = (props) => {
-    const teamData = mock.teamData;
-    const matchLog = mock.matchLog;
-    const ballCount = mock.ballCount;
-    const baseCount = mock.baseCount;
-    const chance = mock.chance;
-    const scoreBoard = mock.scoreBoard;
-    const currentPlayer = mock.currentPlayer;
-
-    const value = {
-        teamData,
-        matchLog,
-        ballCount,
-        baseCount,
-        chance,
-        scoreBoard,
-        currentPlayer,
+    // 임시 mock 데이터
+    const mockData = {
+        teamData: mock.teamData,
+        matchLog: mock.matchLog,
+        ballCount: mock.ballCount,
+        baseCount: mock.baseCount,
+        chance: mock.chance,
+        scoreBoard: mock.scoreBoard,
+        currentPlayer: mock.currentPlayer,
     }
 
     return (
-        <BaseBallContext.Provider {...{ value }}>
+        <BaseBallContext.Provider value={{ ...mockData }}>
             {props.children}
-        </BaseBallContext.Provider>
+        </BaseBallContext.Provider >
     )
 }
 
