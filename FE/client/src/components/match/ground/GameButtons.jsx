264629b0addea7edd63503061f261cfb9e3e2136
sgdg <seungdeng17@gmail.com>
2020-05-09 19:15:49 +0900

    style: SWING, WAIT 버튼 위치 조정

--- FE/client/src/components/match/ground/GameButtons.jsx
+++ FE/client/src/components/match/ground/GameButtons.jsx
@@ -3,16 +3,19 @@ import styled from 'styled-components';
 
 const GameButtonsWrap = styled.div`
     position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 130px;
+    bottom: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 750px;
     display: flex;
-    flex-direction: column;
     justify-content: space-between;
+    z-index: 2;
     & button {
         font-size: 25px;
         width: 220px;
         height: 60px;
+        border-radius: 10px;
+        outline: none;
     }
 `;
 
