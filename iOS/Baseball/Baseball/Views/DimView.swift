8db72a96b8cf46ce8f57576ef5fb6035997a539d
seizze <cwcwc3@gmail.com>
2020-05-11 20:09:56 +0900

    [#22] feat: 매치를 기다리는 상태의 셀 구현

---
+++ iOS/Baseball/Baseball/Views/DimView.swift
@@ -0,0 +1,20 @@
+//
+//  DimView.swift
+//  Baseball
+//
+//  Created by Chaewan Park on 2020/05/11.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class DimView: UIView {
+    
+    func setDimmed() {
+        alpha = 0.5
+    }
+    
+    func setNormal() {
+        alpha = 0
+    }
+}
--- iOS/Baseball/Baseball/Views/MatchCell.swift
+++ iOS/Baseball/Baseball/Views/MatchCell.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+enum MatchState {
+    case normal
+    case waiting
+    case matched
+}
+
 @IBDesignable
 class MatchCell: UICollectionViewCell {
     
     @IBOutlet weak var awayLabel: UILabel!
     @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var dimView: DimView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet { configureView() }
@@ -20,6 +28,10 @@ class MatchCell: UICollectionViewCell {
     
     var match: Match? = nil {
         didSet { configureCell() }
+    }
+    
+    var state: MatchState = .normal {
+        didSet { changeCell(to: state) }
     }
     
     override init(frame: CGRect) {
@@ -30,6 +42,20 @@ class MatchCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureView()
+    }
+    
+    private func changeCell(to state: MatchState) {
+        switch state {
+        case .normal:
+            dimView.setNormal()
+            activityIndicator.isHidden = true
+        case .waiting:
+            dimView.setDimmed()
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+        case .matched:
+            activityIndicator.stopAnimating()
+        }
     }
     
     private func configureView() {
