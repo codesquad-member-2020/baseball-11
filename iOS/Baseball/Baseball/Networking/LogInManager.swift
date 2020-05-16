aa5b49b2def764bfaa1031320a46410b633cf89d
seizze <cwcwc3@gmail.com>
2020-05-16 16:42:46 +0900

    [#29] refactor: OAuth 로그인 매니저 개선

--- iOS/Baseball/Baseball/Networking/LogInManager.swift
+++ iOS/Baseball/Baseball/Networking/LogInManager.swift
@@ -11,30 +11,53 @@ import AuthenticationServices
 
 enum LogInError: Error {
     case tokenNotFound
+    
+    var localizedDescription: String {
+        switch self {
+        case .tokenNotFound: return "JWT Token Not Found"
+        }
+    }
 }
 
-protocol LogInManageable { }
+protocol LogInManageable {
+    var presentationContextProvider: ASWebAuthenticationPresentationContextProviding { get }
+}
 
 extension LogInManageable {
     func requestOAuth(with url: URL?,
                       scheme: String,
+                      payloadKey: String,
                       completion: @escaping (Result<String, Error>) -> Void) {
         guard let url = url else { return }
         
-        ASWebAuthenticationSession(url: url, callbackURLScheme: scheme) { callbackURL, error in
+        let session = ASWebAuthenticationSession(url: url, callbackURLScheme: scheme) { callbackURL, error in
             if let error = error {
                 completion(.failure(error))
                 return
             }
             guard let url = callbackURL else { return }
             if let token = URLComponents(string: url.absoluteString)?
-                .queryItems?.filter({ $0.name == "code" }).first?.value {
+                .queryItems?.filter({ $0.name == payloadKey }).first?.value {
                 completion(.success(token))
             } else {
                 completion(.failure(LogInError.tokenNotFound))
             }
-        }.start()
+        }
+        session.presentationContextProvider = presentationContextProvider
+        session.start()
     }
 }
 
-struct LogInManager: LogInManageable { }
+class LogInManager: LogInManageable {
+    var presentationContextProvider: ASWebAuthenticationPresentationContextProviding
+    
+    init() {
+        presentationContextProvider = PresentationContextProvider()
+    }
+}
+
+class PresentationContextProvider: NSObject, ASWebAuthenticationPresentationContextProviding {
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return ASPresentationAnchor()
+    }
+}
